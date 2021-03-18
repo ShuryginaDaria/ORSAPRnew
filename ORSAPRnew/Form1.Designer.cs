36504725bddbeb978269d67722bb4fec3bc09691
Shurygina Darya <tillmedie@gmail.com>
2021-03-18 20:23:07 +0700

    Внесены изменения формы

--- ORSAPRnew/Form1.Designer.cs
+++ ORSAPRnew/Form1.Designer.cs
@@ -47,27 +47,27 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(221, 32);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 8, 3, 8);
+            this.textBox1.Location = new System.Drawing.Point(249, 40);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 10, 3, 10);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(89, 22);
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(221, 77);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 8, 3, 8);
+            this.textBox2.Location = new System.Drawing.Point(249, 96);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 10, 3, 10);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(89, 22);
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
             this.textBox2.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(7, 32);
-            this.label1.Margin = new System.Windows.Forms.Padding(3, 8, 3, 0);
+            this.label1.Location = new System.Drawing.Point(8, 40);
+            this.label1.Margin = new System.Windows.Forms.Padding(3, 10, 3, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(147, 17);
+            this.label1.Size = new System.Drawing.Size(167, 20);
             this.label1.TabIndex = 2;
             this.label1.Text = "Высота коробки (мм)";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -75,73 +75,73 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(7, 77);
-            this.label2.Margin = new System.Windows.Forms.Padding(3, 8, 3, 0);
+            this.label2.Location = new System.Drawing.Point(8, 96);
+            this.label2.Margin = new System.Windows.Forms.Padding(3, 10, 3, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(141, 17);
+            this.label2.Size = new System.Drawing.Size(159, 20);
             this.label2.TabIndex = 3;
             this.label2.Text = "Длина коробки (мм)";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(7, 127);
-            this.label3.Margin = new System.Windows.Forms.Padding(3, 8, 3, 0);
+            this.label3.Location = new System.Drawing.Point(8, 159);
+            this.label3.Margin = new System.Windows.Forms.Padding(3, 10, 3, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(149, 17);
+            this.label3.Size = new System.Drawing.Size(168, 20);
             this.label3.TabIndex = 5;
             this.label3.Text = "Ширина коробки (мм)";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(221, 127);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 8, 3, 8);
+            this.textBox3.Location = new System.Drawing.Point(249, 159);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 10, 3, 10);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(89, 22);
+            this.textBox3.Size = new System.Drawing.Size(100, 26);
             this.textBox3.TabIndex = 4;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(10, 37);
-            this.label4.Margin = new System.Windows.Forms.Padding(3, 8, 3, 0);
+            this.label4.Location = new System.Drawing.Point(11, 46);
+            this.label4.Margin = new System.Windows.Forms.Padding(3, 10, 3, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(189, 17);
+            this.label4.Size = new System.Drawing.Size(218, 20);
             this.label4.TabIndex = 7;
             this.label4.Text = "Длина каждого отсека (мм)";
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(224, 37);
-            this.textBox4.Margin = new System.Windows.Forms.Padding(3, 8, 3, 8);
+            this.textBox4.Location = new System.Drawing.Point(252, 46);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(3, 10, 3, 10);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(89, 22);
+            this.textBox4.Size = new System.Drawing.Size(100, 26);
             this.textBox4.TabIndex = 6;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(10, 81);
-            this.label5.Margin = new System.Windows.Forms.Padding(3, 8, 3, 0);
+            this.label5.Location = new System.Drawing.Point(11, 101);
+            this.label5.Margin = new System.Windows.Forms.Padding(3, 10, 3, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(197, 17);
+            this.label5.Size = new System.Drawing.Size(227, 20);
             this.label5.TabIndex = 9;
             this.label5.Text = "Ширина каждого отсека (мм)";
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(224, 81);
-            this.textBox5.Margin = new System.Windows.Forms.Padding(3, 8, 3, 8);
+            this.textBox5.Location = new System.Drawing.Point(252, 101);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(3, 10, 3, 10);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(89, 22);
+            this.textBox5.Size = new System.Drawing.Size(100, 26);
             this.textBox5.TabIndex = 8;
             // 
             // BuildButton
             // 
-            this.BuildButton.Location = new System.Drawing.Point(310, 350);
-            this.BuildButton.Margin = new System.Windows.Forms.Padding(3, 2, 4, 4);
+            this.BuildButton.Location = new System.Drawing.Point(268, 431);
+            this.BuildButton.Margin = new System.Windows.Forms.Padding(3, 2, 4, 5);
             this.BuildButton.Name = "BuildButton";
-            this.BuildButton.Size = new System.Drawing.Size(100, 28);
+            this.BuildButton.Size = new System.Drawing.Size(112, 35);
             this.BuildButton.TabIndex = 10;
             this.BuildButton.Text = "Построить";
             this.BuildButton.UseVisualStyleBackColor = true;
@@ -155,11 +155,11 @@
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.textBox3);
             this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Location = new System.Drawing.Point(11, 17);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 8, 3, 2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 21);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 10, 3, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.groupBox1.Size = new System.Drawing.Size(399, 174);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(368, 218);
             this.groupBox1.TabIndex = 11;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Параметры коробки";
@@ -170,29 +170,28 @@
             this.groupBox2.Controls.Add(this.label4);
             this.groupBox2.Controls.Add(this.textBox4);
             this.groupBox2.Controls.Add(this.label5);
-            this.groupBox2.Location = new System.Drawing.Point(8, 201);
-            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 8, 3, 2);
+            this.groupBox2.Location = new System.Drawing.Point(9, 251);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 10, 3, 2);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.groupBox2.Size = new System.Drawing.Size(402, 131);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox2.Size = new System.Drawing.Size(371, 164);
             this.groupBox2.TabIndex = 13;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Параметры отсеков";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CausesValidation = false;
-            this.ClientSize = new System.Drawing.Size(422, 393);
+            this.ClientSize = new System.Drawing.Size(393, 480);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.BuildButton);
-            this.ImeMode = System.Windows.Forms.ImeMode.NoControl;
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(440, 440);
-            this.MinimumSize = new System.Drawing.Size(440, 440);
+            this.MaximumSize = new System.Drawing.Size(415, 536);
+            this.MinimumSize = new System.Drawing.Size(415, 536);
             this.Name = "Form1";
             this.ShowIcon = false;
             this.Text = "Box Builder";
